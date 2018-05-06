ff980d0deea4fe0d64fdb25696ff5766aa979e2d
wley3337@gmail.com <wley3337@gmail.com>
2018-05-06 16:09:01 +0000

    automatically backed up by learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -1,9 +1,15 @@
-def roll_call_dwarves# code an argument here
+def roll_call_dwarves(disney_dont_sue)
   # Your code here
+  disney_dont_sue.each.with_index(1) { |x, index|
+  puts "#{index} #{x}"
+  }
 end
 
-def summon_captain_planet# code an argument here
+def summon_captain_planet(epa)
   # Your code here
+  epa.map { |x|
+  x.capitalize + "!"
+  }
 end
 
 def long_planeteer_calls# code an argument here
