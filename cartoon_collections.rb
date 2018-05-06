b51c68fb1fc49d99c3f5bef682e7d1bcd6c0bdec
wley3337@gmail.com <wley3337@gmail.com>
2018-05-06 16:44:04 +0000

    automatically backed up by learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -13,7 +13,7 @@ def summon_captain_planet(epa)
 end
 
 def long_planeteer_calls(array)
-  array.include? (x.length > 4)
+  array.include? (array.each { |x| x.length < 4 })
   
   # Your code here
 end
