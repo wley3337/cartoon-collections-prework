4ed48397684abac98c756c2e60d47bfba679307e
wley3337@gmail.com <wley3337@gmail.com>
2018-05-06 16:36:42 +0000

    automatically backed up by learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -21,5 +21,8 @@ end
 def find_the_cheese(array)
   # the array below is here to help
   cheese_types = ["cheddar", "gouda", "camembert"]
-  array.find
+  array.find { |x|
+    x == "cheddar" || "gouda" || "camembert"
+  }
+  
 end
