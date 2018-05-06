f6240c6b426070a9926cb989139a8df5d1061a31
wley3337@gmail.com <wley3337@gmail.com>
2018-05-06 17:20:22 +0000

    Done.

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -1,16 +1,30 @@
-def roll_call_dwarves# code an argument here
+def roll_call_dwarves(disney_dont_sue)
+  # Your code here
+  disney_dont_sue.each.with_index(1) { |x, index|
+  puts "#{index} #{x}"
+  }
+end
+
+def summon_captain_planet(epa)
+  # Your code here
+  epa.map { |x|
+  x.capitalize + "!"
+  }
+end
+
+def long_planeteer_calls(array)
+  array.any? { |x|
+  x.length > 4 
+  }
+  
   # Your code here
 end
 
-def summon_captain_planet# code an argument here
-  # Your code here
-end
-
-def long_planeteer_calls# code an argument here
-  # Your code here
-end
-
-def find_the_cheese# code an argument here
+def find_the_cheese(array)
   # the array below is here to help
   cheese_types = ["cheddar", "gouda", "camembert"]
+  array.find { |x|
+    cheese_types.include? (x)
+  }
+  
 end
